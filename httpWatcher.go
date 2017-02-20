bbcb9aa6ea131a1c375f3984ddea4134ad343a2f
Jake Cooper <jake.elijah.cooper@gmail.com>
2017-02-20 00:24:06 -0800

    Forgot to actually add the worker

---
+++ httpWatcher.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+type commandStruct struct {
+	Command string
+}
+
+var conn redis.Conn
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	decoder := json.NewDecoder(r.Body)
+
+	var cmd commandStruct
+	err := decoder.Decode(&cmd)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cmd.Command)
+	//Validate Command
+	//Add it to Redis
+	conn.Do("RPUSH", fmt.Sprintf("worker:%d:pendingtx", *workerNum), cmd.Command)
+
+	responseText := fmt.Sprintf("Successfully performed %s\n", cmd.Command)
+	fmt.Fprintf(w, responseText, r.URL.Path[1:])
+}
+
+func httpWatcher() {
+	conn = redisPool.Get()
+	port := ":6060"
+	fmt.Printf("Started watching on port %s\n", port)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(port, nil)
+}
