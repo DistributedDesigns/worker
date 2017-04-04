15d25d32144b80301a3d08e981afc39888305cd5
Tyler Stephen <tyler.stephen@gmail.com>
2017-04-03 23:29:13 -0700

    Pass back user state in JSON

--- pendingTx.go
+++ pendingTx.go
@@ -1,7 +1,14 @@
 package main
 
+type pendingTxState struct {
+	Stock     string `json:"stock"`
+	Amount    string `json:"amount"`
+	ExpiresAt string `json:"expiresAt"`
+}
+
 type pendingTx interface {
 	Commit()
 	RollBack()
 	IsExpired() bool
+	GetState() pendingTxState
 }
